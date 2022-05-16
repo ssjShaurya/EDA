89430a4f66ef8c38e46dd359072e9cec94077f3c
ssjShaurya <58274024+ssjShaurya@users.noreply.github.com>
2022-05-16 19:16:52 +0530

    Add files via upload

--- EDA-project_1.py
+++ EDA-project_1.py
@@ -179,3 +179,34 @@ sns.distplot(value_stats)
 
 
 
+
+# ## Observations for Year and Trade Value
+# ### -Year
+# - As seen above trade over the years can be catagorized in two broad groups i.e. high_year and low_year
+#   
+#   -High Year
+#   - high_year has more than or equal to 20000 volume traded
+#   - for 20000 to 217000 density of numbers of years is high
+#   - but 21700 and 23000 density decreases
+#   - once we reache 23000 volume traded density increases again
+#   This suggests that trade has been going pretty good as a high density of years have volume traded greater than 23000
+#  
+#   -Low Year
+#   - low_year has less than 20000 volume traded
+#   - 5000 to 15000 volume traded has about the ame density i.e. about equal number of years
+#   - whereas a geater number of years have more than 15000 teaded in vaolume
+#    This suggest that more and more years were begining to incounter good fortune in relation to volume traded
+#    
+#  ### - Trade Value
+#  - Trade Value is taken to be the sum of mone made through trade in one year (for scale 10 million will be treated as 1)
+#    - over the years profit made has steadily increased with some exceptions
+#    - for 0 to 2 the density is extremely high suggesting it was the satrting of trade and every year trade was getting better
+#    - for 2 to 3 the density drops suggesting this much amount was made over a lesser number of years
+#    - according to the bar-graph its between this time period (2008 - 2016) that we saw major fluctuations clueing us to the fact that trade was quite unstable for some time
+#    - after 3.5 the profit began to rise again till 2020 when it fell again, trend continuing into 2021
+
+# In[ ]:
+
+
+
+
